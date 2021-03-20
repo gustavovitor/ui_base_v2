bab9f2ef3c37fa42708f7731691e009025e52afc
Jailson Felipe de Miranda <jailsonfelipem@gmail.com>
2021-03-19 23:31:48 -0300

    CRUD de produtos no frontend - Criar módulo e componentes de produtos

--- src/app/pages/init/init-routing.module.ts
+++ src/app/pages/init/init-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
       {
         path: 'clientes',
         loadChildren: () => import('./clientes/clientes.module').then(m => m.ClientesModule)
+      },
+      {
+        path: 'produtos',
+        loadChildren: () => import('./produtos/produtos.module').then(m => m.ProdutosModule)
       }
     ]
   }
---
+++ src/app/pages/init/produtos/produtos-formulario-page/produtos-formulario-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-produtos-formulario-page',
+  templateUrl: './produtos-formulario-page.component.html',
+  styleUrls: ['./produtos-formulario-page.component.scss']
+})
+export class ProdutosFormularioPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
